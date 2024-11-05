32a7f2d398242036d12016e3cf94fa5c9472da6f
Mateus Colombo Godoy <mateuscgodoy@gmail.com>
2024-11-05 19:34:07 -0300

    refactor: add serialize and question type to IQuestion interface

--- src/lib/IQuestion.ts
+++ src/lib/IQuestion.ts
@@ -1,9 +1,12 @@
-import { QuestionObj } from './questionTypes';
+import { QuestionDBM, QuestionObj } from './questionTypes';
 
 export default interface IQuestion<T> {
   text: string;
   instruction?: string;
   correctAnswer: T;
+  questionType: string;
+  created_at: Date;
   getDisplayQuestion(): QuestionObj;
   checkAnswer(answer: T): boolean;
+  serialize(): QuestionDBM<T>;
 }
