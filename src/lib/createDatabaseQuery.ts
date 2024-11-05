2b599014d4828b75dbb5fb50a9527d6fca7bc894
Mateus Colombo Godoy <mateuscgodoy@gmail.com>
2024-11-05 18:09:26 -0300

    refactor: add .vscode to gitignore and instruction to QuestionObj type

--- src/lib/createDatabaseQuery.ts
+++ src/lib/createDatabaseQuery.ts
@@ -1,10 +1,12 @@
-const createQuestionsQuery = `CREATE TABLE IF NOT EXISTS Questions (
+const createQuestionsQuery = `CREATE TABLE IF NOT EXISTS questions (
 id INTEGER PRIMARY KEY AUTOINCREMENT,
-text TEXT NOT NULL
+text TEXT NOT NULL UNIQUE,
+created_at TEXT 
 );
-CREATE TABLE IF NOT EXISTS Answers (
+CREATE TABLE IF NOT EXISTS alternatives (
 id INTEGER PRIMARY KEY AUTOINCREMENT,
 question_id INTEGER,
+text TEXT NOT NULL,
 is_correct INTEGER NOT NULL,
 FOREIGN KEY (question_id) REFERENCES questions(id)
 );`;
--- src/lib/questionTypes.ts
+++ src/lib/questionTypes.ts
@@ -1,5 +1,6 @@
 export type QuestionObj = {
   text: string;
+  instruction?: string;
   alternatives: string[];
 };
 
