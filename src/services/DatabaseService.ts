42ef80a489cc3de7d7fcc0a3219ae162f320d5ef
Mateus Colombo Godoy <mateuscgodoy@gmail.com>
2024-11-08 12:39:26 -0300

    refactor: change Database Service to only promote Database actions

--- src/services/DatabaseService.ts
+++ src/services/DatabaseService.ts
@@ -1,115 +1,50 @@
-import { DatabaseSync } from 'node:sqlite';
+import { DatabaseSync, SupportedValueType } from 'node:sqlite';
 
-import createQuestionsQuery from '../lib/createDatabaseQuery';
-import {
-  AlternativeDBM,
-  OperationResult,
-  Question,
-} from '../lib/questionTypes';
-import QuestionFactory from '../lib/QuestionFactory';
-import BaseQuestion from '../models/BaseQuestion';
+import { AlternativeDBM, OperationResult } from '../lib/questionTypes';
+import InvalidQuery from '../models/errors/InvalidQuery';
 
 export default class DatabaseService {
   private instance: DatabaseSync;
 
-  constructor(dbPath: string = ':memory:') {
+  constructor(dbPath: string = ':memory:', starterQuery?: string) {
     this.instance = new DatabaseSync(dbPath);
-    this.instance.exec(createQuestionsQuery);
+    if (starterQuery) {
+      this.instance.exec(starterQuery);
+    }
   }
 
-  saveQuestion(question: BaseQuestion<any>): OperationResult {
-    try {
-      const data = question.serialize();
-      console.log(data);
-      const getQuestionType = this.instance.prepare(
-        'SELECT id FROM question_types WHERE name = ?;'
-      );
-
-      let { id } = getQuestionType.get(data.question_type) as {
-        id: number | null;
-      };
-
-      if (!id) {
-        const { lastInsertRowid } = this.instance
-          .prepare('INSERT INTO question_types (name) VALUES (?);')
-          .run(data.question_type);
-        id = lastInsertRowid as number;
-      }
-
-      const { lastInsertRowid: questionId } = this.instance
-        .prepare(
-          'INSERT INTO questions (text, question_type_id, created_at) VALUES (?, ?, ?);'
-        )
-        .run(data.text, id, data.created_at);
-
-      if (Array.isArray(data.correctAnswer)) {
-        for (const answer of data.correctAnswer) {
-          this.insertAlternative({
-            text: answer,
-            question_id: questionId as number,
-            is_correct: 1,
-          });
-        }
-      } else {
-        this.insertAlternative({
-          text: data.correctAnswer,
-          question_id: questionId as number,
-          is_correct: 1,
-        });
-      }
-
-      if (data.otherAlternatives) {
-        for (const other of data.otherAlternatives) {
-          this.insertAlternative({
-            text: other,
-            question_id: questionId as number,
-            is_correct: 0,
-          });
-        }
-      }
-    } catch (error) {
-      return {
-        success: false,
-        error: error as Error,
-        message: 'Save operation failed',
-      };
+  get<T>(query: string, params: SupportedValueType[]): OperationResult<T> {
+    const result = this.instance.prepare(query).get(...params);
+    if (!result) {
+      throw new InvalidQuery('Invalid GET query provided', query);
     }
-    return { success: true, message: 'Question saved with success' };
-  }
-
-  loadQuestion(id: number): OperationResult<BaseQuestion<unknown>> {
-    // Does a question with the provided ID exists on DB? (Don't forget to JOIN with the question_type)
-    const question = this.instance
-      .prepare(
-        'SELECT q.id, q.text, q.created_at, t.name as question_type FROM questions q INNER JOIN question_types t ON q.question_type_id = t.id WHERE q.id = ?;'
-      )
-      .get(id) as Question;
-
-    if (!question.id) {
-      return {
-        success: false,
-        message: 'A question with the provided ID was not found',
-      };
-    }
-
-    const alternatives = this.instance
-      .prepare('SELECT * FROM alternatives WHERE question_id = ?;')
-      .all(question.id) as AlternativeDBM[];
-
-    // Pass all that information to a "QuestionFactory" that will correctly create the proper question
-    const questionInstance = QuestionFactory.build(question, alternatives);
     return {
       success: true,
-      message: 'Question loaded with success',
-      data: questionInstance,
+      message: 'GET query completed',
+      data: result as T,
     };
   }
 
-  private insertAlternative(alternative: AlternativeDBM) {
-    this.instance
-      .prepare(
-        'INSERT INTO alternatives (text, question_id, is_correct) VALUES (?,?,?);'
-      )
-      .run(alternative.text, alternative.question_id, alternative.is_correct);
+  getAll<T>(query: string, params: SupportedValueType[]): OperationResult<T> {
+    const result = this.instance.prepare(query).all(...params);
+    return {
+      success: true,
+      message: 'GET ALL query completed',
+      data: result as T,
+    };
+  }
+
+  insert(query: string, params: SupportedValueType[]) {
+    const statement = this.instance.prepare(query);
+    const { changes, lastInsertRowid } = statement.run(...params);
+    return { newId: lastInsertRowid as number, changes };
+  }
+
+  update() {
+    // TODO Database Service UPDATE method
+  }
+
+  delete() {
+    // TODO Database Service DELETE method
   }
 }
