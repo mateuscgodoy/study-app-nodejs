838addb1f0699f79f853756561304a154f2fa9b9
Mateus Colombo Godoy <mateuscgodoy@gmail.com>
2024-11-08 19:58:46 -0300

    refactor: add try/catch blocks to capture errors from queries

--- src/services/DatabaseService.ts
+++ src/services/DatabaseService.ts
@@ -1,6 +1,6 @@
 import { DatabaseSync, SupportedValueType } from 'node:sqlite';
 
-import { AlternativeDBM, OperationResult } from '../lib/questionTypes';
+import { OperationResult } from '../lib/questionTypes';
 import InvalidQuery from '../models/errors/InvalidQuery';
 
 export default class DatabaseService {
@@ -13,31 +13,72 @@ export default class DatabaseService {
     }
   }
 
-  get<T>(query: string, params: SupportedValueType[]): OperationResult<T> {
-    const result = this.instance.prepare(query).get(...params);
-    if (!result) {
-      throw new InvalidQuery('Invalid GET query provided', query);
+  get<T>(query: string, params: SupportedValueType[] = []): OperationResult<T> {
+    try {
+      const result = this.instance.prepare(query).get(...params);
+
+      if (!result) {
+        throw new InvalidQuery('Invalid GET query provided', query);
+      }
+      return {
+        success: true,
+        message: 'GET query completed',
+        data: result as T,
+      };
+    } catch (error) {
+      return {
+        success: false,
+        message: `GET operation failed with message: '${
+          (error as Error).message
+        }'`,
+        error: error as Error,
+      };
     }
-    return {
-      success: true,
-      message: 'GET query completed',
-      data: result as T,
-    };
   }
 
-  getAll<T>(query: string, params: SupportedValueType[]): OperationResult<T> {
-    const result = this.instance.prepare(query).all(...params);
-    return {
-      success: true,
-      message: 'GET ALL query completed',
-      data: result as T,
-    };
+  getAll<T>(
+    query: string,
+    params: SupportedValueType[] = []
+  ): OperationResult<T> {
+    try {
+      const result = this.instance.prepare(query).all(...params);
+      return {
+        success: true,
+        message: 'GET ALL query completed',
+        data: result as T,
+      };
+    } catch (error) {
+      return {
+        success: false,
+        message: `INSERT operation failed with message: '${
+          (error as Error).message
+        }'`,
+        error: error as Error,
+      };
+    }
   }
 
-  insert(query: string, params: SupportedValueType[]) {
-    const statement = this.instance.prepare(query);
-    const { changes, lastInsertRowid } = statement.run(...params);
-    return { newId: lastInsertRowid as number, changes };
+  insert(
+    query: string,
+    params: SupportedValueType[] = []
+  ): OperationResult<{ newId: number; changes: number }> {
+    try {
+      const statement = this.instance.prepare(query);
+      const { changes, lastInsertRowid } = statement.run(...params);
+      return {
+        success: true,
+        message: 'INSERT query completed',
+        data: { newId: lastInsertRowid as number, changes: changes as number },
+      };
+    } catch (error) {
+      return {
+        success: false,
+        message: `INSERT operation failed with message: '${
+          (error as Error).message
+        }'`,
+        error: error as Error,
+      };
+    }
   }
 
   update() {
