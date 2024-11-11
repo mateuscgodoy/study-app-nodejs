04d9956fe95ca0c6464a918410f8b6bddac53090
Mateus Colombo Godoy <mateuscgodoy@gmail.com>
2024-11-11 20:20:26 -0300

    refactor: BaseQuestion abstract class using new structure

--- src/models/questions/BaseQuestion.ts
+++ src/models/questions/BaseQuestion.ts
@@ -1,36 +1,30 @@
 import DIFFICULTY from '../../lib/enums/difficulty';
 import QUESTION_TYPES from '../../lib/enums/questionTypes';
+import Serializable from '../../lib/interfaces/Serializable';
 import {
   QuestionDBM,
-  Question,
+  QuestionData,
   QuestionDisplay,
 } from '../../types/question.types';
-import Alternative from './Alternative';
 
-export default abstract class BaseQuestion {
+export default abstract class BaseQuestion
+  implements Serializable<QuestionDBM>
+{
   protected text: string;
-  protected alternatives: Alternative[];
-  protected tags: string[];
+  // protected alternatives: Alternative[];
   protected createdAt: Date;
   protected difficulty: DIFFICULTY | null;
 
-  constructor(question: Question, createdAt?: Date) {
+  constructor(question: QuestionData, createdAt?: Date) {
     this.text = question.text;
-    this.alternatives = question.alternatives;
-    this.tags = question.tags ?? [];
+    // this.alternatives = question.alternatives;
     this.createdAt = createdAt ?? new Date();
     this.difficulty = question.difficulty ?? null;
   }
 
-  checkAnswer(answer: string): boolean {
-    return this.alternatives.some(
-      (alt) =>
-        alt.text.toLocaleLowerCase() === answer.toLocaleLowerCase() &&
-        alt.isCorrect
-    );
-  }
   abstract display(): QuestionDisplay;
-  abstract serialize(): QuestionDBM; // ! This might need a new place
+  abstract serialize(): QuestionDBM;
+  abstract deserialize(value: QuestionDBM): void;
 
   static getType(): QUESTION_TYPES {
     return QUESTION_TYPES.NONE;
